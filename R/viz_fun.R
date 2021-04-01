a7b3386526209eeea94d833214a66f18b63172b3
Tumino <giorgio.tumino@wur.nl>
2021-04-01 14:33:06 +0200

    merge conflict in skyplot

--- R/viz_fun.R
+++ R/viz_fun.R
@@ -244,22 +244,20 @@ QQ.plot <- function( pvals, ylim = NULL, plot_legend = T, legnames=NULL, coltype
 #' @export
 #'
 skyplot<-function( pval, map, threshold = NULL, ylab = NULL, xlab = NULL, ylim=NULL, chrom = NULL,
-<<<<<<< HEAD
-                   small = NULL, col = NULL, h = NULL, l = NULL, pch = NULL, cex.big = NULL,
+                   small = NULL, col = NULL, h = NULL, l = NULL, pch = NULL, chromspace = 0.05, cex.big = NULL,
                    cex.small = NULL, line.big = NULL, ...
-){
-  #In case the markers are not in order
-  # map <- map[with(map,order(map$chromosome,map$position)),]
-=======
-                    small = NULL, col = NULL, h = NULL, l = NULL, pch = NULL, chromspace = 0.05, ...
 ){
   #In case the markers are not in order
   neworder <- order(map$chromosome,map$position)
   map <- map[neworder,]
   #any marker order change must be applied to pval accordingly,
   #since map and pval are expected to be provided in the same marker order
-  pval <- pval[neworder]
->>>>>>> 250adb70a1ce95c7f585a3928dfc840b7af7186e
+  if(!is.null(names(pval))){
+    pval <- pval[map$marker]
+  }else{
+    pval <- pval[neworder]
+  }
+
 
   #We filter per chromosome
   if(is.null(chrom)) chrom <- as.character(unique(map$chromosome))
