206e6785d71e2cd94d49b97fe866b8678505366a
Tumino <giorgio.tumino@wur.nl>
2023-04-04 16:29:06 +0200

    Submission to CRAN
    Former-commit-id: c2957b77986f4b8e58708ef854e2c4ba081a7cc4 [formerly 74b96231d966281848fe1400946473e8bb54081e]
    Former-commit-id: d6019d8e488d0a3e61a2f756745669cc6f6bcc34

--- devtest/package_build.R
+++ devtest/package_build.R
@@ -1,12 +1,18 @@
-
 devtools::load_all()
+
+# Add citation -------------------
+usethis::use_citation()
+
+
+
+
 
 
 # Package building ------------------------------------
 # library(devtools)
 
 ## function documentation --------------
-devtools::document()
+# devtools::document()
 devtools::check_man() #calls document() and runs the checks for manual included in check_built()
 # devtools::build_manual() #to add a pdf manual
 
@@ -24,14 +30,15 @@ devtools::check()
 devtools::check_win_devel()
 
 ## build -----------------
-devtools::build()
+# devtools::build()
+devtools::build(vignettes = F) #skip building vignettes
 
 ## check after building -------------
-devtools::check_built(paste0("../mpQTL_0.6.1.tar.gz"), cran=TRUE)
+devtools::check_built(paste0("../mpQTL_0.6.2.tar.gz"), cran=TRUE)
 
 
 # setwd("D:/Science/PhD/Code/mpQTL/mpQTL/")
 # build(vignettes = F)
 
 # #To install it
-# install.packages("mpQTL_0.5.0.tar.gz", repos = NULL, type = "source")
+# install.packages("../mpQTL_0.6.2.tar.gz", repos = NULL, type = "source")
